6f0a3fa989b86610f186da23fccabcbcba38726c
Alvin Schnee <59372520+Alvin-Schnee@users.noreply.github.com>
2026-05-28 06:45:20 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,35 +1,37 @@
 {
-	description = "Minimal NixOS router/dev setup";
-	
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  description = "Minimal NixOS with optional Hyprland module";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.11";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	
-		hyprland.url = "github:hyprwm/Hyprland";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-	outputs = { self, nixpkgs, home-manager, hyprland, ... }:
-    	let
-      		system = "x86_64-linux";
-    	in {
-      		nixosConfigurations.router = nixpkgs.lib.nixosSystem {
-        	inherit system;
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-        	modules = [
-          		./hosts/router/configuration.nix
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
 
-          		home-manager.nixosModules.home-manager
-          		{
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-            
-            		home-manager.users.alvin = import ./home/alvin.nix;
-          		}
-      		];
-    	};
-  	};
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.router = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [
+        ./hosts/router/configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users._0x4rch3typ3 = import ./home/_0x4rch3typ3.nix;
+        }
+
+        # /!\ Remove this module should Hyprland be not needed anymore !
+        ./modules/hyprland.nix
+      ];
+    };
+  };
 }
